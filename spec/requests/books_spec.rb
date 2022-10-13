4260f503d8bd8fcbdcb3f12cb0b4b28b8d384a06
Luis Hong <luishong50@gmail.com>
2022-10-13 15:35:02 -0500

    added unit testing for the post and delete enpoints

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,12 +1,43 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
-    it 'returns all books' do
-        FactoryBot.create(:book, title: 'Harry Potter', author: 'JKR')
-        FactoryBot.create(:book, title: 'Sandman', author: 'Neil Gaiman')
-        get '/api/v1/books'
+    describe 'GET /books' do
+        before do 
+            FactoryBot.create(:book, title: 'Harry Potter', author: 'JKR')
+            FactoryBot.create(:book, title: 'Sandman', author: 'Neil Gaiman')
+        end
+        
+        it 'returns all books' do
+            get '/api/v1/books'
+    
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body).size).to eq(2)
+        end
+    
+    end
 
-        expect(response).to have_http_status(:success)
-        expect(JSON.parse(response.body).size).to eq(2)
+    describe 'POST /books' do
+        it 'creates a new book' do
+            expect {
+                post '/api/v1/books', params: {book: {title: 'Don Quijote', author: 'Cervantes'}}
+
+        }.to change {Book.count}.from(0).to(1)
+
+            expect(response).to have_http_status(:created)
+        end
+    end
+
+    describe 'DELETE /books/:id' do
+        let!(:book) {FactoryBot.create(:book, title: '1984', author: 'Orwell')}
+        it 'deletes a book' do
+            expect {
+                delete "/api/v1/books/#{book.id}"
+                
+        }.to change {Book.count}.from(1).to(0)
+
+            expect(response).to have_http_status(:no_content)
+        end
     end
 end 
+
+ 
